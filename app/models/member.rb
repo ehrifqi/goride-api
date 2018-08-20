c800fd5b5576c6ae227046d105f4156f368ed14e
Fernando Christyanto <christyantofernando@gmail.com>
2018-08-20 09:55:10 +0700

    Member model validations and spec

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,8 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {in: 6..20}
+  validates :username, length: {maximum: 30}, allow_blank:true
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @member1 = create(:member)
+    @member2 = create(:member)
+    @member3 = create(:member)
+  end
+  it "has a valid factory" do
+    expect(@member1).to be_valid
+  end
+  
+  it "is invalid without an email" do
+    @member1.email = nil
+    expect(@member1).to_not be_valid
+  end
+
+  it "is invalid with a duplicate email" do
+    @member2.email = @member1.email
+    expect(@member2).to_not be_valid
+  end
+
+  it "is invalid without a password" do
+    @member1.password = nil
+    expect(@member1).to_not be_valid
+  end
+
+  it "is valid with a username" do
+    expect(@member1).to be_valid
+  end
+
+  it "is valid with no username" do
+    @member1.username = nil
+    @member2.username = ""
+    expect(@member1).to be_valid
+    expect(@member2).to be_valid
+  end
+
+  it "is invalid with a username with length greater than 30" do
+    @member1.username = "toolongusernamethismustwillnotpass"
+    expect(@member1).to_not be_valid
+  end
+
+  it "is invalid with password length lower than 6 or greater than 20" do
+    @member1.password = "short"
+    expect(@member1).to_not be_valid
+  end
+
+  it "is valid with password length between 6 and 20" do
+    @member1.password = "validpass"
+    expect(@member1).to be_valid
+  end
 end
