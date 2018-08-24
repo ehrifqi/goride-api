1fdd9816dfb923477c53f33ecf488050cdacefbd
Fernando Christyanto <christyantofernando@gmail.com>
2018-08-24 23:24:16 +0700

    Fix member & driver seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ Driver.delete_all
 Member.create({
   email: "member@gmail.com",
   password: "member",
+  full_name: "Member One",
   phone_number: "08561856245",
   go_pay_value: 1000,
   go_point_value: 1000
@@ -17,7 +18,8 @@ Driver.create({
   license_plate: "B1511BNZ",
   rating: 4.9,
   phone_number: "08561657562",
-  license_number: "B 1672 BNZ",
+  license_plate: "B 1672 BNZ",
+  license_number: "123456789012",
   ktp_number: "1234567890123456",
   address: "Syahdan baru",
   income: 0
